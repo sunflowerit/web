ed2814221adeadf36bdb5a971d73266e4e68f63c
hayyan <hayyan-ebrahem@hotmail.com>
2017-12-18 08:37:10 +0100

    changes and bugfixes to support_branding

---
+++ support_freshfilter/__openerp__.py
@@ -0,0 +1,15 @@
+# -*- encoding: utf-8 -*-
+{
+    'name': 'Support Freshfilter Sunflower IT',
+    'version': '8.0',
+    'category': 'Support',
+    'license': 'AGPL-3',
+    'summary': 'Support adaptions for Freshfilter',
+    'author': 'Sunflower IT',
+    'website': 'http://sunflowerweb.nl/',
+    'depends': ['support_branding'],
+    'data': [
+      'support_branding_data.xml',
+    ],
+    'installable': True
+}
